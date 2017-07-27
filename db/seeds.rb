25426946dd4f375cab873d934029e782b13c31b2
Natali <dblack0000@gmail.com>
2017-07-27 04:24:50 +0300

    Seeded database with random emails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+1.upto(10271) do |i|
+  SignupQueue.create(
+    email: "randomemailnumber#{i}@nonexistingdomain.com",
+    is_email_confirmed: true)
+end
